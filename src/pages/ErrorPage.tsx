cde55778cffad555a49f06a8604306d8ec88a047
Nikola Dojcinovic <goonergalactico@gmail.com>
2024-05-25 00:14:02 +0200

    error page styling

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -7,12 +7,17 @@ export const ErrorPage = () => {
   return (
     <>
       <Navbar />
-      <div>
-        <p>
-          {isRouteErrorResponse(error)
-            ? "This page doesnot exist"
-            : "An unespected error occured "}
-        </p>
+      <div className="flex justify-center items-center h-[calc(100vh_-_100px)]">
+        {isRouteErrorResponse(error) ? (
+          <div className="flex items-center">
+            <p className="text-2xl font-semibold p-4 border-r border-white/30">
+              404
+            </p>
+            <p className="p-4">This page doesn't exist.</p>
+          </div>
+        ) : (
+          <p>An unespected error occured.</p>
+        )}
       </div>
     </>
   );
