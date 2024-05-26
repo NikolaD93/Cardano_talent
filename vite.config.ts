edcc93f8253bffb70bd6109d6977a942c97d0d9b
Nikola Dojcinovic <goonergalactico@gmail.com>
2024-05-26 02:04:08 +0200

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  define: {
-    "process.env": loadEnv(process.cwd(), ""),
-  },
+    define: {
+      "process.env": env,
+    },
+  };
 });
