f8c0a65417f572ff4e040b5bdc66c958e986a07c
Nikola Dojcinovic <goonergalactico@gmail.com>
2024-05-26 02:14:40 +0200

    Correct vite.config.ts File

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -13,10 +13,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     define: {
-      "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
-        env.VITE_SUPABASE_URL
-      ),
-      "import.meta.env.VITE_ANON_KEY": JSON.stringify(env.VITE_ANON_KEY),
+      "process.env": env,
     },
   };
 });
