fe185ffc99b37605b31b2c2048ef0e62227947e2
Nikola Dojcinovic <goonergalactico@gmail.com>
2024-05-26 02:11:38 +0200

    fixx

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -13,7 +13,10 @@ export default defineConfig(({ mode }) => {
       },
     },
     define: {
-      "process.env": env,
+      "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
+        env.VITE_SUPABASE_URL
+      ),
+      "import.meta.env.VITE_ANON_KEY": JSON.stringify(env.VITE_ANON_KEY),
     },
   };
 });
